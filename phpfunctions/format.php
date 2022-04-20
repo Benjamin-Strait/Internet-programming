ad99e050d1162ac964b642d92fdfa40d6ee96710
tcchalk0 <103468955+tcchalk0@users.noreply.github.com>
2022-04-20 12:36:00 -0400

    Add files via upload

---
+++ phpfunctions/format.php
@@ -0,0 +1,78 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="./css/formatStyle.css">
+</head>
+<body>
+
+<?php 
+  function pageHeader($title, $img)
+  {
+    echo<<<EOT
+    <div class="navbar">
+    <img src=$img width="175" height="100">
+    <h1>$title</h1>
+      <div class="dropdown" onmouseleave="hideMenu()">
+          <button class="dropbtn" onmouseover="showMenu()" onclick="showMenu()">Menu
+            <i class="fa fa-caret-down"></i>
+          </button>
+        <div class="dropdown-content" id="Dropdown">
+          <a href="#">Home</a>
+          <a href="#">Forum</a>
+          <a href="#">Store</a>
+          <a href="#">Cart <i class="fa fa-shopping-cart"></i></a>
+        
+    EOT;
+    if(!isset($_SESSION['name']))
+    {
+      echo<<<EOT
+      <a href="#">Signup</a>
+      <a href="#">Login</a>
+      </div>
+      </div> 
+      </div>
+      EOT;
+    }
+    else
+    {
+      echo<<<EOT
+      <a href="#">Logout</a>
+      </div>
+      </div> 
+      </div>      
+      EOT;
+    }
+    
+    
+  }
+
+  function pageFooter()
+  {
+
+  }
+?>
+
+<script type="text/javascript" src = "./js/formatJS.js"></script>
+<script>
+  window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+</script>
+
+</body>
+</html>
