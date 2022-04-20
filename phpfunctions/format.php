bb3355d4803f63b78877245eba4645dd2cf52570
tcchalk0 <103468955+tcchalk0@users.noreply.github.com>
2022-04-20 13:26:20 -0400

    Update format.php

--- phpfunctions/format.php
+++ phpfunctions/format.php
@@ -7,7 +7,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="../css/formatStyle.css">
+<link rel="stylesheet" href="./css/formatStyle.css">
 </head>
 <body>
 
@@ -58,7 +58,7 @@
   }
 ?>
 
-<script type="text/javascript" src = "../js/formatJS.js"></script>
+<script type="text/javascript" src = "./js/formatJS.js"></script>
 <script>
   window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
