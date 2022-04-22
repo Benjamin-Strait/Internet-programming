a7266231cea0869c98e6857da4ef03bab7d442ff
tcchalk0 <103468955+tcchalk0@users.noreply.github.com>
2022-04-22 14:07:28 -0400

    header working with bootstrap

--- phpfunctions/format.php
+++ phpfunctions/format.php
@@ -8,6 +8,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="./css/formatStyle.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
 
@@ -15,25 +16,33 @@
   function pageHeader($title, $img)
   {
     echo<<<EOT
-    <div class="navbar">
-    <img src=$img width="175" height="100">
-    <h1>$title</h1>
-      <div class="dropdown" onmouseleave="hideMenu()">
-          <button class="dropbtn" onmouseover="showMenu()" onclick="showMenu()">Menu
-            <i class="fa fa-caret-down"></i>
-          </button>
-        <div class="dropdown-content" id="Dropdown">
-          <a href="#">Home</a>
-          <a href="#">Forum</a>
-          <a href="./store.php">Store</a>
-          <a href="./cart.php">Cart <i class="fa fa-shopping-cart"></i></a>
+    <div class="navbar navbar-nav">
+    <div class="row">
+    <div class="col-6">
+      <img id="head-logo" src=$img width="175" height="100">
+    </div>
+    <div class="col-4">
+      <h1 id="head-title">$title</h1>
+    </div>
+    </div>
+    
+    <div class="btn-group">
+      <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Menu
+      </button>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Home</a>
+        <a class="dropdown-item" href="#">Forum</a>
+        <a class="dropdown-item" href="./store.php">Store</a>
+        <a class="dropdown-item" href="./cart.php">Cart <i class="fa fa-shopping-cart"></i></a>
+  
         
     EOT;
     if(!isset($_SESSION['name']))
     {
       echo<<<EOT
-      <a href="./register.php">Signup</a>
-      <a href="./login.php">Login</a>
+      <a class="dropdown-item" href="./register.php">Signup</a>
+      <a class="dropdown-item" href="./login.php">Login</a>
       </div>
       </div> 
       </div>
@@ -42,10 +51,10 @@
     else
     {
       echo<<<EOT
-      <a href="#">Logout</a>
+      <a class="dropdown-item" href="./phpfunctions/logout.php">Logout</a>
       </div>
       </div> 
-      </div>      
+      </div>
       EOT;
     }
     
